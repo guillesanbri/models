b3baa85d992a37f1230a71614360ce1d2cc64d07
guillesanbri <guillesanbri@hotmail.com>
2021-06-16 17:18:32 +0200

    added dataset to data_generator

--- research/deeplab/datasets/data_generator.py
+++ research/deeplab/datasets/data_generator.py
@@ -99,10 +99,20 @@ _ADE20K_INFORMATION = DatasetDescriptor(
     ignore_label=0,
 )
 
+_GAUZES_INFORMATION = DatasetDescriptor(
+    splits_to_sizes={
+        'train': 3019,  # num of samples in images/training
+        'val': 984,  # num of samples in images/validation
+    },
+    num_classes=2,
+    ignore_label=0,
+)
+
 _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
     'ade20k': _ADE20K_INFORMATION,
+    'gauzes': _GAUZES_INFORMATION,
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
